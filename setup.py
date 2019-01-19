02ba9ac7da0c1344181f6a29dd1d8f3fae5c33e4
kumokiri2kdev <kumokiri2i@gmail.com>
2019-01-20 08:55:29 +0900

    [ADD]Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='k2keiba',
+    version='0.0.1',
+    packages=['k2kparser'],
+    url='',
+    license='free',
+    author='kumokiri2k',
+    author_email='',
+    description='k2keiba JRA data parser'
+)
