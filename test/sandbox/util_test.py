84ce3ccd1999f4f7d5cadcbabd9ccf6d39ccb054
kumokiri2kdev <kumokiri2i@gmail.com>
2022-09-11 13:07:57 +0900

    [DEL]Delete test/sandbox

--- test/sandbox/util_test.py
+++
@@ -1,9 +0,0 @@
-
-from k2kparser import util as ut
-
-
-print(ut.Util.parse_date_mmdd_to_int('1月22日', 2022))
-print(ut.Util.parse_date_mmdd_to_int('1月22日'))
-
-print(ut.Util.parse_date_mmdd_to_int('11月29日'))
-print(ut.Util.parse_date_mmdd_to_int('11月29日', 2021))
