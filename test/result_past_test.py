28a802e147d24b739d13bad2aa72e92adc6c30c9
kumokiri2kdev <ubuntu@ip-172-31-11-90.ap-northeast-1.compute.internal>
2026-02-28 05:12:12 +0000

    [MOD]Update result parameter

--- test/result_past_test.py
+++ test/result_past_test.py
@@ -10,8 +10,8 @@ if __name__ == '__main__':
     logging.config.fileConfig('logging.ini',disable_existing_loggers=False )
     logger = logging.getLogger(__name__)
 
-    current = 202212
-    end = 202212
+    current = 202501
+    end = 202602
 
     while current <= end:
         param = prp.ParserRaceParams.get_cname(current)
